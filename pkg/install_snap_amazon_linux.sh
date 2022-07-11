1d4d65b94489d1ee098f38f0972072538069e3c3
EC2 Default User <ec2-user@ip-10-12-69-170.us-west-2.compute.internal>
2022-07-11 22:02:20 +0000

    Auto git update july 2022 - tony

--- pkg/install_snap_amazon_linux.sh
+++ pkg/install_snap_amazon_linux.sh
@@ -2,7 +2,7 @@
 
 sudo yum install git
 
-sudo 'cd /etc/yum.repos.d && wget https://people.canonical.com/~mvo/snapd/amazon-linux2/snapd-amzn2.repo.'
+sudo bash -c 'cd /etc/yum.repos.d && wget https://people.canonical.com/~mvo/snapd/amazon-linux2/snapd-amzn2.repo'
 
 sudo yum install snapd
 
